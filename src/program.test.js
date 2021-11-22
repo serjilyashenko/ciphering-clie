82596967ddc44829ac2ff149b5888d8473679941
Serge Ilyashenko <serj.ilyashenko@gmail.com>
2021-11-22 22:27:46 +0300

    test cases

--- src/program.test.js
+++ src/program.test.js
@@ -10,3 +10,14 @@ test('Ciphers from file to console', async () => {
 
   expect(stdout).toBe('This is secret. Message about "_" symbol!\n')
 })
+
+test('Throws error if I miss -c', async () => {
+  try {
+    await exec('node ./index.js -i __fixtures__/input.txt')
+  } catch (error) {
+    // eslint-disable-next-line jest/no-conditional-expect
+    expect(error.message).toContain('-c')
+    // eslint-disable-next-line jest/no-conditional-expect
+    expect(error.message).toContain('--config')
+  }
+})
